b192293b9a5948df30ef460c67eef8918ec43ebb
THEdarkkman <103695837+THEdarkkman@users.noreply.github.com>
2024-05-17 20:05:04 +0200

    Updated mss-Test.py Optimize code Added PIL

--- mss-Test.py
+++ mss-Test.py
@@ -4,35 +4,106 @@ from PIL import Image
 import pyautogui
 import time
 
-# Timer, return the time upon call
 def timer():
+    """
+    Start a timer
+
+    Returns:
+        float: return the time
+    """
     return time.time()
 
-# Locate an element on the screen
 def locateOnScreen(filename, confidence=0.7):
+    """
+    Locate an element on the screen based on an image
+
+    Args:
+        filename (string): String containing image path
+        confidence (float, optional): Determine the confidence. Defaults to 0.7.
+
+    Returns:
+        int: Return coordinate, width and height of the element
+    """
     posX, posY, width, height = pyautogui.locateOnScreen(filename, confidence=confidence)
     return int(posX), int(posY), int(width), int(height)
 
-# Take a screenshot of the region
-def screenshot(top, left, width, height):
+def screenshot(posX, posY, width, height):
+    """
+    Take a screenshot of a certain region
+
+    Args:
+        posX (_type_): Left most position
+        posY (_type_): Top most position
+        width (_type_): Width desired
+        height (_type_): height desired
+
+    Returns:
+        image: standard image
+    """
     with mss.mss() as sct:
         monitor = {
-            "left" : left,
-            "top" : top,
+            "left" : posX,
+            "top" : posY,
             "width" : width,
             "height" : height
         }
         screenshot = sct.grab(monitor)
-        mss.tools.to_png(screenshot.rgb, screenshot.size, output="img/mss-test.png")
-        return sct.grab(monitor)
+        #mss.tools.to_png(screenshot.rgb, screenshot.size, output="img/mss-test.png")
+        return screenshot
     
-# Convert screenshot to PIL image and grayscale
 def convertToPILImage(screenshot):
+    """ 
+    Convert an image to a PIL Image
+
+    Args:
+        screenshot (image): standard image
+
+    Returns:
+        image: PIL Image (Grayscale)
+    """
     # Convert screenshot to PIL image
     img = Image.frombytes('RGB', screenshot.size, screenshot.bgra, "raw", "BGRX")
     # Convert to grayscale
     return img.convert("L")
 
+def detectPixelInRange(pixelList, minValue=97, maxValue=105):
+    """
+    Detect pixel values within a specified range.
+
+    Args:
+        pixelList (list of int): List of grayscale pixel values.
+        minValue (int): Minimum value of the range (inclusive).
+        maxValue (int): Maximum value of the range (inclusive).
+
+    Returns:
+        list of int: List of positions where pixel values are within the range.
+    """
+    positions = []
+    
+    for i, value in enumerate(pixelList):
+        if minValue <= value <= maxValue:
+            positions.append(i)
+    
+    return positions
+
+def getPixelValuePIL(img):
+    """
+    Get the pixel value within an image 
+
+    Returns:
+        list of ints: Return a list of pixel value
+    """
+    # Get pixel value into list
+    pixelValues = []
+    # For each pixel within the width
+    for y in range(img.width):
+        # Get pixel value
+        pixelValue = img.getpixel((y, 0))
+        # Store pixel value
+        pixelValues.append(pixelValue)
+    return pixelValues
+        
+        
 # Find the left and right hand
 leftHand = locateOnScreen("img/leftHand.png")
 rightHand = locateOnScreen("img/rightHand.png")
@@ -42,21 +113,15 @@ left = leftHand[0] + leftHand[2]
 top = leftHand[1] + 10
 width = rightHand[0] - rightHand[2] - leftHand[0]
 height = 1
-pyautogui.moveTo(leftHand[0] + leftHand[2], leftHand[1] + 10)
-pyautogui.moveTo(rightHand[0], leftHand[1]+10)
-# Take screenshot based on region
+
+# Take a screenshot of the region
 img = screenshot(left, top, width, height)
-    
+
+# Convert the image into a pil grayscale image
 imgGray = convertToPILImage(img)
+imgGray.save("img/test.png")
 
-imgGray.save("test.png")
-
-# Get pixel value into list
-pixelValues = []
-# For each pixel within the width
-for y in range(imgGray.width):
-    # Get pixel value
-    pixelValue = imgGray.getpixel((y, 0))
-    # Store pixel value
-    pixelValues.append(pixelValue)
+print(detectPixelInRange(getPixelValuePIL(imgGray)))
+    
+#print(pixelValues)
 
